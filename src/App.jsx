4087b51f2dcede3854b12b281d41ae832a166389
Drew Tullos <tullos.andrew@gmail.com>
2024-01-07 22:27:26 -0600

    Page loading

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,28 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import AboutMe from "./sections/AboutMe";
-import Portfolio from "./sections/Portfolio";
+// import Portfolio from "./sections/Portfolio";
 import Contact from "./sections/Contact";
-import Resume from "./sections/Resume";
-import Footer from "./components/Footer";
-import "./App.css";
+// import Resume from "./sections/Resume";
+// import Footer from "./components/Footer";
+// import "./App.css";
 
 function App() {
 	return (
 		<Router>
-			<Header />
-			<main>
-				<Routes>
-					<Route path="/" element={<AboutMe />} />
-					<Route path="/portfolio" element={<Portfolio />} />
-					<Route path="/contact" element={<Contact />} />
-					<Route path="/resume" element={<Resume />} />
-				</Routes>
-			</main>
-			<Footer />
+			<div className="min-h-screen bg-light-green">
+				<Header />
+				<main>
+					<Routes>
+						<Route path="/" />
+						<Route path="/about" element={<AboutMe />} />
+						{/* <Route path="/portfolio" element={<Portfolio />} /> */}
+						<Route path="/contact" element={<Contact />} />
+						{/* <Route path="/resume" element={<Resume />} /> */}
+					</Routes>
+				</main>
+				{/* <Footer /> */}
+			</div>
 		</Router>
 	);
 }
