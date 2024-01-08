c8a536bb00ab01bd7b79a1a10728f13d3cde99c2
Drew Tullos <tullos.andrew@gmail.com>
2024-01-07 19:15:08 -0600

    Main layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"dark-blue": "#05386B",
+				"green-blue": "#379683",
+				"light-green": "#5CDB95", // Main background color
+				"mint-green": "#8EE4AF",
+				"off-white": "#EDF5E1",
+			},
+		},
+	},
+	plugins: [],
+};
