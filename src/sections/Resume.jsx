26a0834884f667a1e96aaeeda5240eab3955550b
Drew Tullos <tullos.andrew@gmail.com>
2024-01-08 21:25:16 -0600

    Netlify image error fix

--- src/sections/Resume.jsx
+++ src/sections/Resume.jsx
@@ -66,7 +66,7 @@ export default function Example() {
 				<div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
 					<img
 						className="w-[48rem] max-w-none rounded-xl bg-dark-blue shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
-						src="./src/assets/ME.jpg"
+						src="./src/assets/me.jpg"
 						alt=""
 					/>
 				</div>
